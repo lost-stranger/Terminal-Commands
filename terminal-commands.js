a2d9ad845459989bfea4c2cdeb2c60654adc41a1
Arun Milton <miltononline@gmail.com>
2019-02-16 13:24:57 -0500

    assignment complete

--- terminal-commands.js
+++ terminal-commands.js
@@ -1,11 +1,29 @@
-const fs = require('fs');
+const fs = require('fs')
 
 module.exports.ls = () => {
-   fs.readdir('./', (err, files) => {
-     const filesToString = files.reduce((acc, file) => {
+  fs.readdir('./', (err, files) => {
+    const filesToString = files.reduce((acc, file) => {
       return `${acc} ${file} `;
     }, '');
 
     console.log(filesToString);
-  };
+  });
+};
+
+module.exports.touch = () => {
+  fs.appendFile('greetings', 'Hello again world!', function(err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('file created!');
+  });
+};
+
+module.exports.mkdir = () => {
+  fs.mkdir('/BLOC/bloc/terminal-commands/test',function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Directory created successfully!");
+ });
 };
